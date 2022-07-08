9038bd7407b192ac35457af19fbeef74e6df629b
moroz-art-dev <moroz.art.dev@gmail.com>
2022-07-08 03:48:06 +0300

    HOC

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Message from "./components/Message";
 
-const initialState = {
-  name: "Manny",
-  message: "Typescript cool!"
-}
-
-type State = Readonly<typeof initialState>;
-
-class App extends Component<any, State> {
-  readonly state: State = initialState;
+class App extends Component {
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
-          <Message name={this.state.name} message={this.state.message}/>
+          <Message />
         </header>
       </div>
     );
